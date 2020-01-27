faaf747d5c288c1b236361b317d277a22d678c56
Neil FARMER <43957137+Neilstid@users.noreply.github.com>
2020-01-27 14:14:52 +0100

    Add files via upload

--- GameOfLife/main.cpp
+++ GameOfLife/main.cpp
@@ -1,24 +1,31 @@
 #include "GameOfLife.h" // Inclure le fichier .h
 #include <iostream>
+#include <unistd.h>
 
 int main()
 {
-    std::cout<<"\tJeu de la vie"<<std::endl;
-    int HauteurGrille, LargeurGrille;
+    std::cout<<"\t\t\t\t\t\t\tJeu de la vie"<<std::endl;
 
+    /*
+    int Choix_Implementation;
+    std::cout<<"Choix implementation :\n [1] : Utiliser schema predefini\n [2] : Creer son propre schema\n Choix : ";
+    std::cin>>Choix_Implementation;
+    */
+
+    int HauteurGrille, LargeurGrille;
     std::cout<<"Taille de la grille : \n Hauteur : ";
     std::cin>>HauteurGrille;
     std::cout<<" Largeur : ";
     std::cin>>LargeurGrille;
-
-    GameOfLife game(10, 20); // définition d'un objet "game" de taille 50x50
-    game.affiche_grille(); //Affiche la grille creer
+    GameOfLife game(HauteurGrille, LargeurGrille); // définition d'un objet "game" de taille 50x50
     game.initialisation(); //Initialise la grille
 
-    for(int i = 0; i<5;i++){
+    game.GrilleDefault(3);
+
+    for(int i = 0; i<1000;i++){
         game.play();//Tour suivant
         game.affiche_grille(); //Affiche la grille
-        std::cout<<std::endl; //Saut de ligne
+        usleep(50000);//Temps affichage entre chaque update
     }
   return 0;
 }
--- GameOfLife/src/GameOfLife.cpp
+++ GameOfLife/src/GameOfLife.cpp
@@ -1,15 +1,18 @@
 #include "GameOfLife.h" // Inclure le fichier .h
 #include <iostream>
+#include <stdlib.h>
 
 // Constructeur
 GameOfLife::GameOfLife(int numLines, int numCols) {
     _numLines = numLines;//Nombre de ligne de la grille
     _numCols = numCols;//Nombre de colonne de la grille
 
+    /*
     //Affiche les parametre choisi
     std::cout<<std::endl<<"Parametre :"<<std::endl;
     std::cout<<"Nombre de ligne =" << numLines <<std::endl;
     std::cout<<"Nombre de colonne =" << numCols <<std::endl<<std::endl;
+    */
 
     //met toute la grille a 0 avant de choisir les celulle vivante
     for (int Fill_Ligne = 0; Fill_Ligne < numLines; Fill_Ligne++){//Parcours les lignes
@@ -21,6 +24,10 @@ GameOfLife::GameOfLife(int numLines, int numCols) {
 
         }
     }
+
+}
+
+GameOfLife::GameOfLife(){
 
 }
 
@@ -138,14 +145,17 @@ int GameOfLife::nbAlivedNeighbours(int i, int j) {
 
 //Affiche la grille actuel
 void GameOfLife::affiche_grille(){
+
+    system("CLS");//clear le terminal
+
     //Parcours la grille
     for (int Fill_Ligne = 0; Fill_Ligne < _numLines; Fill_Ligne++){
         for (int Fill_Colonne = 0; Fill_Colonne < _numCols; Fill_Colonne++){
 
             if(_grid[Fill_Ligne][Fill_Colonne] == 1){
-                std::cout<<"*";//Cellule vivante
+                std::cout<<"* ";//Cellule vivante
             }else{
-                std::cout<<".";//Cellule morte
+                std::cout<<" ";//Cellule morte
             }
 
         }
@@ -157,20 +167,75 @@ void GameOfLife::affiche_grille(){
 void GameOfLife::GrilleDefault(int choixGrille){
     switch(choixGrille){//choisi la bonne grille en fonction du choix de l'utilisateur
     case 1 ://situation stable tour 4
-        _grid[5][7] = 1;
-        _OldGrid[5][7] = 1;
-        _grid[5][8] = 1;
-        _OldGrid[5][8] = 1;
-        _grid[6][8] = 1;
-        _OldGrid[6][8] = 1;
-        _grid[5][9] = 1;
-        _OldGrid[5][9] = 1;
-        _grid[5][10] = 1;
-        _OldGrid[5][10] = 1;
-        _grid[5][11] = 1;
-        _OldGrid[5][11] = 1;
-        break;
+        {
+            ResizeGrille(10,20);
+            _grid[5][7] = 1;
+            _OldGrid[5][7] = 1;
+            _grid[5][8] = 1;
+            _OldGrid[5][8] = 1;
+            _grid[6][8] = 1;
+            _OldGrid[6][8] = 1;
+            _grid[5][9] = 1;
+            _OldGrid[5][9] = 1;
+            _grid[5][10] = 1;
+            _OldGrid[5][10] = 1;
+            _grid[5][11] = 1;
+            _OldGrid[5][11] = 1;
+            break;
+        }
+    case 2 :
+        {
+             ResizeGrille(10,20);
+            _grid[5][7] = 1;
+            _OldGrid[5][7] = 1;
+            _grid[5][8] = 1;
+            _OldGrid[5][8] = 1;
+            _grid[5][9] = 1;
+            _OldGrid[5][9] = 1;
+            break;
+        }
+    case 3 :
+        {
+             ResizeGrille(40,85);
+            _grid[20][40] = 1;
+            _OldGrid[20][40] = 1;
+            _grid[20][41] = 1;
+            _OldGrid[20][41] = 1;
+            _grid[21][41] = 1;
+            _OldGrid[21][41] = 1;
+            _grid[21][42] = 1;
+            _OldGrid[21][42] = 1;
+            _grid[22][42] = 1;
+            _OldGrid[22][42] = 1;
+            _grid[22][43] = 1;
+            _OldGrid[22][43] = 1;
+            break;
+        }
     default://Cas par default du switch
         break;
+    }
+}
+
+//Permet de redimensionner la grille
+void GameOfLife::ResizeGrille(int numLines, int numCols){
+    _numLines = numLines;//Nombre de ligne de la grille
+    _numCols = numCols;//Nombre de colonne de la grille
+
+    /*
+    //Affiche les parametre choisi
+    std::cout<<std::endl<<"Parametre :"<<std::endl;
+    std::cout<<"Nombre de ligne =" << numLines <<std::endl;
+    std::cout<<"Nombre de colonne =" << numCols <<std::endl<<std::endl;
+    */
+
+    //met toute la grille a 0 avant de choisir les celulle vivante
+    for (int Fill_Ligne = 0; Fill_Ligne < numLines; Fill_Ligne++){//Parcours les lignes
+        for (int Fill_Colonne = 0; Fill_Colonne < numCols; Fill_Colonne++){//Parcours les colonnes
+
+            //Initialise les 2 grilles
+            _grid[Fill_Ligne][Fill_Colonne] = 0; //Met les valeurs a 0
+            _OldGrid[Fill_Ligne][Fill_Colonne] = 0; //Met les valeurs a 0
+
+        }
     }
 }
